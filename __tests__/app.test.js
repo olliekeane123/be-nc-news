368de82add3ad02e8c632d2feb94088ee2da7b4a
olliekeane123 <ollie.keane1@gmail.com>
2024-11-25 11:33:54 +0000

    set up test suite with supertest and correct file structure

--- __tests__/app.test.js
+++ __tests__/app.test.js
@@ -1,15 +1,28 @@
-const endpointsJson = require("../endpoints.json");
+const request = require("supertest")
+const endpointsJson = require("../endpoints.json")
+const app = require("../mvc/app")
+const db = require("../db/connection")
+const seed = require("../db/seeds/seed")
+const data = require("../db/data/test-data/index")
+
+afterAll(() => {
+    return db.end()
+})
+beforeEach(() => {
+    return seed(data)
+})
+
 /* Set up your test imports here */
 
 /* Set up your beforeEach & afterAll functions here */
 
 describe("GET /api", () => {
-  test("200: Responds with an object detailing the documentation for each endpoint", () => {
-    return request(app)
-      .get("/api")
-      .expect(200)
-      .then(({ body: { endpoints } }) => {
-        expect(endpoints).toEqual(endpointsJson);
-      });
-  });
-});
+    test("200: Responds with an object detailing the documentation for each endpoint", () => {
+        return request(app)
+            .get("/api")
+            .expect(200)
+            .then(({ body: { endpoints } }) => {
+                expect(endpoints).toEqual(endpointsJson)
+            })
+    })
+})
