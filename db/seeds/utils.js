24adc0be15d7c17b266f4efe9294c14e2411f458
olliekeane123 <ollie.keane1@gmail.com>
2024-11-27 18:47:40 +0000

    test anc complete 12-GET-api/articles-topic

--- db/seeds/utils.js
+++ db/seeds/utils.js
@@ -20,3 +20,37 @@ exports.formatComments = (comments, idLookup) => {
         }
     })
 }
+
+exports.validateQueries = (sort_by, order, topic) => {
+    const allowedSortByValues = [
+        "title",
+        "topic",
+        "author",
+        "created_at",
+        "votes",
+        "article_img_url",
+        "comment_count",
+    ]
+    const allowedOrderValues = ["ASC", "asc", "DESC", "desc"]
+    const allowedTopicValues = ["mitch", "cats"]
+
+    if (sort_by && !allowedSortByValues.includes(sort_by)) {
+        return Promise.reject({
+            status: 400,
+            msg: "Bad Request: Invalid Sort_By Query",
+        })
+    }
+    if (order && !allowedOrderValues.includes(order)) {
+        return Promise.reject({
+            status: 400,
+            msg: "Bad Request: Invalid Order Query",
+        })
+    }
+    if (topic && !allowedTopicValues.includes(topic)) {
+        return Promise.reject({
+            status: 400,
+            msg: "Bad Request: Invalid Topic Query",
+        })
+    }
+    return Promise.resolve()
+}
--- sql_testing/sql-test-pad.sql
+++ sql_testing/sql-test-pad.sql
@@ -1,5 +1,1 @@
-SELECT articles.title, articles.topic, articles.author, articles.created_at, articles.votes, articles.article_img_url, CAST(COUNT(comments.article_id) AS INT) AS comment_count
-        FROM articles
-        LEFT JOIN comments ON articles.article_id = comments.article_id
-        WHERE articles.article_id = 
-        GROUP BY articles.article_id;
+SELECT * FROM articles WHERE article_id = 3
