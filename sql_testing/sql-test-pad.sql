bb69a05b25eb159e32bae00933428d96fec5607e
olliekeane123 <ollie.keane1@gmail.com>
2024-11-27 19:07:42 +0000

    add comment_count to the returned object of get article by ID

--- sql_testing/sql-test-pad.sql
+++ sql_testing/sql-test-pad.sql
@@ -1,1 +1,6 @@
-SELECT * FROM articles WHERE article_id = 3
+SELECT articles.*, CAST(COUNT(comments.article_id) AS INT) AS comment_count
+        FROM articles
+        LEFT JOIN comments ON articles.article_id = comments.article_id
+        WHERE articles.article_id = %L 
+        GROUP BY articles.article_id
+        
