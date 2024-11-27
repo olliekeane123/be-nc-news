7e4c373608b128d3c227f37bc7469faa9d6c24d1
olliekeane123 <ollie.keane1@gmail.com>
2024-11-27 23:07:12 +0000

    set up files for hosting

--- listen.js
+++ listen.js
@@ -1,5 +1,4 @@
-const app = require("./mvc/app");
+const app = require("./mvc/app")
+const { PORT = 8000 } = process.env
 
-app.listen(8000, () => {
-  console.log("Server is running on port 8000");
-});
+app.listen(PORT, () => console.log(`Listening on ${PORT}...`))
